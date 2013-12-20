0896400d31de20b802656a4714b8ae7a66672fed
lihw <lihw81@gmail.com>
2013-12-20 12:58:33 +0800

    On branch master (milestone)
    - work on texturing of a blade

---
+++ demos/grass/tools/shapemaker/grassblade.h
@@ -0,0 +1,53 @@
+// grassblade.h
+//
+// Created at 2013/12/11
+//
+// Hongwei Li hongwei.li@amd.com
+// All rights reserved
+
+#ifndef GRASSBLADE_H
+#define GRASSBLADE_H
+
+#include <glf/glf.h>
+
+
+class GrassBlade 
+{
+public:
+    GrassBlade();
+    ~GrassBlade();
+
+    void resetShape();
+
+    virtual void render(GLuint times);
+
+    glm::vec3 getKnotPosition(GLint i) const;
+    void setKnotPosition(GLint i, const glm::vec3& position);
+
+    void serialize(std::string& text);
+    void unserialize(const std::string& text);
+
+private:
+    void updateGeometry();
+    void updateVertexBuffer();
+
+    struct Knot
+    {
+        glm::vec3 position;
+        glm::vec3 normal;
+        glm::vec3 color;
+        GLfloat distance;
+        glm::vec3 tangent;
+    };
+
+private:
+    bool              m_dirty;
+    GLint             m_numKnots;
+    Knot*             m_knots;
+    GLfloat           m_length;
+    GLuint*           m_indices;
+    glf::VertexArray  m_vertexArray;
+};
+
+
+#endif // !GRASSBLADE_H
---
+++ demos/grass/tools/shapemaker/renderer.h
@@ -0,0 +1,70 @@
+// renderer.h
+//
+// Created at 2013/11/25
+//
+// Hongwei Li hongwei.li@amd.com
+// All rights reserved
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <glf/glf.h>
+
+class GrassBlade;
+
+class Renderer : public GLFAbstractRenderer
+{
+    friend class Control;
+
+public:
+    Renderer();
+    virtual ~Renderer();
+
+    virtual bool initialize();
+    virtual void cleanup();
+    virtual void render();
+
+    virtual void onMouseButtonDown(int x, int y, int buttons, int modifiers);
+    virtual void onMouseButtonUp(int x, int y, int buttons, int modifiers);
+    virtual void onMouseMove(int x, int y, int buttons, int modifiers);
+    virtual void onMouseWheel(int numDegrees);
+
+    virtual void onResized(int w, int h);
+
+private:
+    enum { NUM_BLADES = 10 };
+
+    glf::Drawable*  m_grid;
+    glf::Shader     m_gridShader;
+    bool            m_showGrid;
+
+    glf::RectColor* m_background;
+
+    glf::Shader     m_grassBladeEditShader;
+    glf::Shader     m_grassBladeKnotShader;
+    glf::Shader     m_grassBladeNormalShader;
+    glf::Shader     m_grassBladeViewShader;
+    GrassBlade*     m_grassBlades[NUM_BLADES];
+    bool            m_showNormal;
+    GLint           m_currentBladeIndex;
+    GLint           m_currentBladeKnotIndex;
+
+    enum
+    {
+        VIEW_MODE,
+        EDIT_MODE,
+    }              m_mode;
+
+
+    bool           m_selfRotating;
+    glf::Camera    m_editCamera;
+    glf::Camera    m_viewCamera;
+    glf::Camera    m_viewSelfRotatingCamera;
+    
+    glf::Arcball   m_arcball;
+    GLfloat        m_rotation;
+    GLint          m_width;
+    GLint          m_height;
+};
+
+#endif // !RENDERER_H
